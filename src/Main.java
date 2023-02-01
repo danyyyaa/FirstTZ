a52913e28edee1c86f7617ac959ba4b07a8ff5e5
danya <danissim00o@yandex.ru>
2023-02-01 20:52:00 +0700

    почти доделал

--- src/Main.java
+++ src/Main.java
@@ -12,15 +12,43 @@ public class Main {
         }*/
 
         String content = "";
-        List<Integer> listContent = new ArrayList<>();
-
         content = getString("C:\\Users\\hp\\projects\\TZ\\src\\in1.txt", content);
         content = getString("C:\\Users\\hp\\projects\\TZ\\src\\in2.txt", content);
         content = getString("C:\\Users\\hp\\projects\\TZ\\src\\in3.txt", content);
 
-
         String[] lines = content.split(("\\r?\\n"));
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
+
+
+        List<Integer> elements = new ArrayList<>();
+
+        for (int i = 0; i < lines.length; i++) {
+            try {
+                elements.add((Integer.parseInt(lines[i])));
+            } catch (NumberFormatException e) {
+                //System.out.println("fsdg");
+            }
+        }
+        int[] arrayElements = new int[elements.size()];
+
+        for (int i = 0; i < arrayElements.length; i++) {
+            arrayElements[i] = elements.get(i);
+        }
+
+        arrayElements = mergeSort(arrayElements);
+        System.out.println("-----------------");
+
+        for (int el : arrayElements) {
+            System.out.println(el);
+        }
+
+
+        /*String[] lines = content.split(("\\r?\\n"));
         int[] elements = new int[lines.length];
+
 
         for (int i = 0; i < elements.length; i++) {
             elements[i] = (Integer.parseInt(lines[i]));
@@ -29,35 +57,54 @@ public class Main {
         for (int el : elements) {
             System.out.println(el);
         }
-        mergeSort(elements);
         System.out.println("-----------------");
+
+        elements = mergeSort(elements);
 
         for (int el : elements) {
             System.out.println(el);
+        }*/
         }
 
-        //mergeSort(elements);
 
-    }
     public static String getString(String path, String content) {
         FileReader fileReader = new FileReader();
         content += fileReader.readFileContents(path) + "\n";
         return content;
     }
 
-    public static void mergeSort(int[] sortArr) {
-        int j;
-        //сортировку начинаем со второго элемента, т.к. считается, что первый элемент уже отсортирован
-        for (int i = 1; i < sortArr.length; i++) {
-            //сохраняем ссылку на индекс предыдущего элемента
-            int swap = sortArr[i];
-            for (j = i; j > 0 && swap < sortArr[j - 1]; j--) {
-                //элементы отсортированного сегмента перемещаем вперёд, если они больше элемента для вставки
-                sortArr[j] = sortArr[j - 1];
-            }
-            sortArr[j] = swap;
-        }
+    public static int[] mergeSort(int[] sortArr) {
+        int[] buffer1 = Arrays.copyOf(sortArr, sortArr.length);
+        int[] buffer2 = new int[sortArr.length];
+        int[] result = mergeSortInner(buffer1, buffer2, 0, sortArr.length);
+
+        return result;
     }
 
+    public static int[] mergeSortInner(int[] buffer1, int[] buffer2, int startIndex, int endIndex) {
+        if (startIndex >= endIndex - 1) {
+            return buffer1;
+        }
 
+        //уже отсортирован
+        int middle = startIndex + (endIndex - startIndex) / 2;
+        int[] sorted1 = mergeSortInner(buffer1, buffer2, startIndex, middle);
+        int[] sorted2 = mergeSortInner(buffer1, buffer2, middle, endIndex);
+
+        //слияние
+        int index1 = startIndex;
+        int index2 = middle;
+        int destIndex = startIndex;
+        int[] result = sorted1 == buffer1 ? buffer2 : buffer1;
+        while (index1 < middle && index2 < endIndex) {
+            result[destIndex++] = sorted1[index1] < sorted2[index2] ? sorted1[index1++] : sorted2[index2++];
+        }
+        while (index1 < middle) {
+            result[destIndex++] = sorted1[index1++];
+        }
+        while (index2 < endIndex) {
+            result[destIndex++] = sorted2[index2++];
+        }
+        return result;
+    }
 }
