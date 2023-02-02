35a4d152919c75b32cb2e1150490d618a29181a9
danya <danissim00o@yandex.ru>
2023-02-02 17:20:22 +0700

    добавил классы

--- src/Main.java
+++ src/Main.java
@@ -1,97 +1,10 @@
-import java.io.FileWriter;
+import sort.IntegerSort;
+
 import java.io.IOException;
-import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String content = "";
-
-        /*for (int i = 0; i < args.length; i++) {
-            content = getString(args[i], content);
-        }*/
-
-        content = getString("src/in1.txt", content);
-        content = getString("src/in2.txt", content);
-        content = getString("src/in3.txt", content);
-
-
-        System.out.println(content); // вывод неотсортированных данных
-
-        String[] lines = content.split(("\\r?\\n"));
-
-        List<Integer> elements = new ArrayList<>();
-
-        for (int i = 0; i < lines.length; i++) {
-            try {
-                elements.add((Integer.parseInt(lines[i])));
-            } catch (NumberFormatException e) {} // проверка на строку в целочисленных файлах
-        }
-
-        int[] arrayElements = new int[elements.size()];
-
-        for (int i = 0; i < arrayElements.length; i++) {
-            arrayElements[i] = elements.get(i);
-        }
-
-        arrayElements = mergeSort(arrayElements);
-
-        //System.out.println("-----------------");
-        /*for (int el : arrayElements) { // вывод отсортированных данных
-            System.out.println(el);
-        }*/
-
-        FileWriter writer = new FileWriter("output.txt");
-        for (int el : arrayElements) {
-            writer.write(el + System.getProperty("line.separator"));
-        }
-        writer.close();
-    }
-
-
-    public static String getString(String path, String content) {
-        FileReader fileReader = new FileReader();
-        content += fileReader.readFileContents(path) + "\n";
-        return content;
-    }
-
-    public static int[] mergeSort(int[] sortArr) {
-        int[] buffer1 = Arrays.copyOf(sortArr, sortArr.length);
-        int[] buffer2 = new int[sortArr.length];
-        int[] result = mergeSortInner(buffer1, buffer2, 0, sortArr.length);
-
-        return result;
-    }
-
-    public static int[] mergeSortInner(int[] buffer1, int[] buffer2, int startIndex, int endIndex) {
-        if (startIndex >= endIndex - 1) {
-            return buffer1;
-        }
-
-        //уже отсортирован
-        int middle = startIndex + (endIndex - startIndex) / 2;
-        int[] sorted1 = mergeSortInner(buffer1, buffer2, startIndex, middle);
-        int[] sorted2 = mergeSortInner(buffer1, buffer2, middle, endIndex);
-
-        //слияние
-        int index1 = startIndex;
-        int index2 = middle;
-        int destIndex = startIndex;
-        int[] result = sorted1 == buffer1 ? buffer2 : buffer1;
-        while (index1 < middle && index2 < endIndex) {
-            result[destIndex++] = sorted1[index1] < sorted2[index2] ? sorted1[index1++] : sorted2[index2++];
-        }
-        while (index1 < middle) {
-            result[destIndex++] = sorted1[index1++];
-        }
-        while (index2 < endIndex) {
-            result[destIndex++] = sorted2[index2++];
-        }
-        return result;
+        IntegerSort integerSort = new IntegerSort();
+        integerSort.run();
     }
 }
---
+++ src/sort/IntegerSort.java
@@ -0,0 +1,112 @@
+package sort;
+
+import util.FileReader;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class IntegerSort {
+    static String content;
+    List<Integer> elements;
+    int[] arrayElements;
+
+    public IntegerSort(String[] args) {
+        try {
+            for (int i = 0; i < args.length; i++) {
+                this.content = getString(args[i], content);
+            }
+            String[] lines = content.split(("\\r?\\n"));
+
+            this.elements = new ArrayList<>();
+
+            for (int i = 0; i < lines.length; i++) {
+                try {
+                    this.elements.add((Integer.parseInt(lines[i])));
+                } catch (NumberFormatException e) {} // проверка на строку в целочисленных файлах
+            }
+            arrayElements = new int[elements.size()];
+
+            for (int i = 0; i < arrayElements.length; i++) {
+                arrayElements[i] = elements.get(i);
+            }
+        } catch (NullPointerException e) {
+            System.out.println("Ошибка. Не переданы файлы");
+            System.exit(0);
+        }
+    }
+    public IntegerSort() {
+        content = "";
+        this.content = getString("src/in1.txt", content);
+        this.content = getString("src/in2.txt", content);
+        this.content = getString("src/in3.txt", content);
+
+        String[] lines = content.split(("\\r?\\n"));
+
+        this.elements = new ArrayList<>();
+
+        for (int i = 0; i < lines.length; i++) {
+            try {
+                this.elements.add((Integer.parseInt(lines[i])));
+            } catch (NumberFormatException e) {} // проверка на строку в целочисленных файлах
+        }
+        arrayElements = new int[elements.size()];
+
+        for (int i = 0; i < arrayElements.length; i++) {
+            arrayElements[i] = elements.get(i);
+        }
+    }
+    public void run() throws IOException {
+        arrayElements = mergeSort(arrayElements);
+
+        FileWriter writer = new FileWriter("output.txt");
+        for (int el : arrayElements) {
+            System.out.println(el); // вывод отсортированного файла
+            writer.write(el + System.getProperty("line.separator"));
+        }
+        writer.close();
+
+    }
+
+    public String getString(String path, String content) {
+        FileReader fileReader = new FileReader();
+        content += fileReader.readFileContents(path) + "\n";
+        return content;
+    }
+    public static int[] mergeSort(int[] sortArr) {
+        int[] buffer1 = Arrays.copyOf(sortArr, sortArr.length);
+        int[] buffer2 = new int[sortArr.length];
+        int[] result = mergeSortInner(buffer1, buffer2, 0, sortArr.length);
+
+        return result;
+    }
+
+    public static int[] mergeSortInner(int[] buffer1, int[] buffer2, int startIndex, int endIndex) {
+        if (startIndex >= endIndex - 1) {
+            return buffer1;
+        }
+
+        //уже отсортирован
+        int middle = startIndex + (endIndex - startIndex) / 2;
+        int[] sorted1 = mergeSortInner(buffer1, buffer2, startIndex, middle);
+        int[] sorted2 = mergeSortInner(buffer1, buffer2, middle, endIndex);
+
+        //слияние
+        int index1 = startIndex;
+        int index2 = middle;
+        int destIndex = startIndex;
+        int[] result = sorted1 == buffer1 ? buffer2 : buffer1;
+        while (index1 < middle && index2 < endIndex) {
+            result[destIndex++] = sorted1[index1] < sorted2[index2] ? sorted1[index1++] : sorted2[index2++];
+        }
+        while (index1 < middle) {
+            result[destIndex++] = sorted1[index1++];
+        }
+        while (index2 < endIndex) {
+            result[destIndex++] = sorted2[index2++];
+        }
+        return result;
+    }
+}
